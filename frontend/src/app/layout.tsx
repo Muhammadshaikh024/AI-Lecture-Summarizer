1333d985f963708acbf4a5f1e8722b337f274d8c
Muhammad Zubair Shaikh <Muhammadawd4@gmail.com>
2026-04-24 19:18:39 +0500

    first commit

--- frontend/src/app/layout.tsx
+++
@@ -1,8 +0,0 @@
-import "./globals.css";
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body style={{ fontFamily: "Arial, sans-serif", margin: 0, padding: 24 }}>{children}</body>
-    </html>
-  );
-}
